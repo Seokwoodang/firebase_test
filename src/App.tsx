27de80360d7a3c0500b35ceb84fe0a80719ddf50
Seokwoodang <swoo1429@naver.com>
2024-04-16 16:51:30 +0900

    code for tuturing

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import styled from "styled-components";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <StTest>
+      <div className="mainFrame">
+        <div className="leftFrame">사진</div>
+        <div className="rightFrame">
+          <div className="upperContent">소개글</div>
+          <div className="bottomContent">해시태그</div>
+        </div>
+      </div>
+    </StTest>
   );
 }
+
+const StTest = styled.div`
+  .mainFrame {
+    display: flex;
+    gap: 20px;
+    width: 100%;
+    .leftFrame {
+      width: 500px;
+      height: 600px;
+      background-color: red;
+    }
+    .rightFrame {
+      display: flex; // 가로 정렬을 하겠다.
+      flex-direction: column; // 세로 정렬 하겠다.
+      width: 800px;
+      height: 600px;
+      background-color: green;
+      gap: 50px;
+      .upperContent {
+        width: 800px;
+        height: 500px;
+        background-color: blue;
+      }
+      .bottomContent {
+        width: 800px;
+        height: 50px;
+        background-color: purple;
+      }
+    }
+  }
+`;
 
 export default App;
