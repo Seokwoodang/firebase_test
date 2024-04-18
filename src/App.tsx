0afbd6a6cd968bc8220b67b00b044ac33b1d8d3c
Seokwoodang <swoo1429@naver.com>
2024-04-18 13:48:35 +0900

    test for gpg

--- src/App.tsx
+++ src/App.tsx
@@ -8,14 +8,11 @@ function App() {
     <StTest>
       <div className="mainFrame">
         <div className="leftFrame">
-                    <img src="https://static.wikia.nocookie.net/starwars/images/c/cc/Star-wars-logo-new-tall.jpg/revision/latest?cb=20210703000259&path-prefix=ko" />
+          <img src="https://static.wikia.nocookie.net/starwars/images/c/cc/Star-wars-logo-new-tall.jpg/revision/latest?cb=20210703000259&path-prefix=ko" />
           <p>성함</p>
         </div>
-        <div className="rightFrame">
-          멤버 표시Name="upperContent"소개글</div>
-          <div className="bottomContent">해시태그</div>
-
-        </div>
+        <div className="rightFrame">멤버 표시Name="upperContent"소개글</div>
+        <div className="bottomContent">해시태그</div>
       </div>
     </StTest>
   );
